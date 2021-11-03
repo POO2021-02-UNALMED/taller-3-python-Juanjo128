0ef96f09b0e849bcdfd7e446fff862bec944d69f
Juanjo128 <53496444+Juanjo128@users.noreply.github.com>
2021-11-02 21:41:42 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -3,33 +3,33 @@ class Control:
     def __init__(self):
         self.tv
 
-        def enlazar(self, tv):
-            self.tv = tv
-            self.tv.setControl(self)
+    def enlazar(self, tv):
+        self.tv = tv
+        self.tv.setControl(self)
 
-        def turnOn(self):
-            self.tv.turnOn()
+    def turnOn(self):
+        self.tv.turnOn()
 
-        def turnOff(self):
-            self.tv.turnOff()
+    def turnOff(self):
+        self.tv.turnOff()
 
-        def canalUp(self):
-            self.tv.canalUp()
+    def canalUp(self):
+        self.tv.canalUp()
 
-        def canaldown(self):
-            self.tv.canalDown()
+    def canaldown(self):
+        self.tv.canalDown()
 
-        def volumenUp(self):
-            self.tv.volumenUp()
+    def volumenUp(self):
+        self.tv.volumenUp()
 
-        def volumenDown(self):
-            self.tv.volumenDown()
+    def volumenDown(self):
+        self.tv.volumenDown()
 
-        def setCanal(self, num):
-            self.tv.setCanal(num)
+    def setCanal(self, num):
+        self.tv.setCanal(num)
 
-        def GetTV(self):
-            return self.tv
+    def GetTV(self):
+        return self.tv
 
-        def SetTV(self, tv):
-            self.tv = tv    
+    def SetTV(self, tv):
+        self.tv = tv    
